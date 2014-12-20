18a7980d3e26d7c66affb1c4749ab583b6e5d284
Dylan Kinnett <dylan@nocategories.net>
2014-12-19 22:27:41 -0500

    FIXES #38

--- lib/extras.php
+++ lib/extras.php
@@ -241,3 +241,111 @@ function cfpff_fallback_quote($post) {
   }
   return $post->post_content;
 }
+
+//defining the filter that will be used to select posts by 'post formats'
+function add_post_formats_filter_to_post_administration(){
+ 
+    //execute only on the 'post' content type
+    global $post_type;
+    if($post_type == 'post'){
+ 
+        $post_formats_args = array(
+            'show_option_all'   => 'All Post formats',
+            'orderby'           => 'NAME',
+            'order'             => 'ASC',
+            'name'              => 'post_format_admin_filter',
+            'taxonomy'          => 'post_format'
+        );
+ 
+        //if we have a post format already selected, ensure that its value is set to be selected
+        if(isset($_GET['post_format_admin_filter'])){
+            $post_formats_args['selected'] = sanitize_text_field($_GET['post_format_admin_filter']);
+        }
+ 
+        wp_dropdown_categories($post_formats_args);
+ 
+    }
+}
+add_action('restrict_manage_posts','add_post_formats_filter_to_post_administration');
+
+//defining the filter that will be used so we can select posts by 'author'
+function add_author_filter_to_posts_administration(){
+ 
+    //execute only on the 'post' content type
+    global $post_type;
+    if($post_type == 'post'){
+ 
+        //get a listing of all users that are 'author' or above
+        $user_args = array(
+            'show_option_all'   => 'All Users',
+            'orderby'           => 'display_name',
+            'order'             => 'ASC',
+            'name'              => 'aurthor_admin_filter',
+            'who'               => 'authors',
+            'include_selected'  => true
+        );
+ 
+        //determine if we have selected a user to be filtered by already
+        if(isset($_GET['aurthor_admin_filter'])){
+            //set the selected value to the value of the author
+            $user_args['selected'] = (int)sanitize_text_field($_GET['aurthor_admin_filter']);
+        }
+ 
+        //display the users as a drop down
+        wp_dropdown_users($user_args);
+    }
+ 
+}
+add_action('restrict_manage_posts','add_author_filter_to_posts_administration');
+
+//restrict the posts by the chosen post format
+function add_post_format_filter_to_posts($query){
+ 
+    global $post_type, $pagenow;
+ 
+    //if we are currently on the edit screen of the post type listings
+    if($pagenow == 'edit.php' && $post_type == 'post'){
+        if(isset($_GET['post_format_admin_filter'])){
+ 
+            //get the desired post format
+            $post_format = sanitize_text_field($_GET['post_format_admin_filter']);
+            //if the post format is not 0 (which means all)
+            if($post_format != 0){
+ 
+                $query->query_vars['tax_query'] = array(
+                    array(
+                        'taxonomy'  => 'post_format',
+                        'field'     => 'ID',
+                        'terms'     => array($post_format)
+                    )
+                );
+ 
+            }
+        }
+    }   
+}
+add_action('pre_get_posts','add_post_format_filter_to_posts');
+
+//restrict the posts by an additional author filter
+function add_author_filter_to_posts_query($query){
+ 
+    global $post_type, $pagenow; 
+ 
+    //if we are currently on the edit screen of the post type listings
+    if($pagenow == 'edit.php' && $post_type == 'post'){
+ 
+        if(isset($_GET['aurthor_admin_filter'])){
+ 
+            //set the query variable for 'author' to the desired value
+            $author_id = sanitize_text_field($_GET['aurthor_admin_filter']);
+ 
+            //if the author is not 0 (meaning all)
+            if($author_id != 0){
+                $query->query_vars['author'] = $author_id;
+            }
+ 
+        }
+    }
+}
+ 
+add_action('pre_get_posts','add_author_filter_to_posts_query');
