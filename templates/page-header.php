915f0fafa7863875651d8181cdaefecf655a9926
Dylan Kinnett <dylan@nocategories.net>
2015-01-01 11:52:06 -0500

    EDIT PAGE LINK

--- templates/page-header.php
+++ templates/page-header.php
@@ -1,5 +1,8 @@
 <?php if /* this it not home */ (! is_home()) { ?>
 <div class="page-header">
+<p class="byline author vcard">
+	<?php the_category( ', ', ''); ?>  <?php edit_post_link('EDIT'); ?>
+</p>
   <h1>
     <?php echo roots_title(); ?>
   </h1>
